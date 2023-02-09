a830b3f5492b20c42dbb8791de14bb1638801651
Parsa Jokar <prsjokar@gmail.com>
2023-02-09 20:16:33 +0330

    CF4A

---
+++ codeforces/4A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    uint16_t w;
+    cin >> w;
+
+    if (!(w & 1) && w != 2)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return (0 ^ 0);
+}
