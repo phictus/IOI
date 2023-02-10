62d1de8f0dac59ad2d3202c15f1ce5c21c8df0dd
Parsa Jokar <prsjokar@gmail.com>
2023-02-10 15:15:12 +0330

    CF50A

---
+++ codeforces/50A.cpp
@@ -0,0 +1,14 @@
+// Parsa Jokar 2023 https://github.com/phictus/ioi
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    uint32_t n, m;
+    cin >> n >> m;
+    cout << (n * m) / 2 << endl;
+
+    return (0 ^ 0);
+}
