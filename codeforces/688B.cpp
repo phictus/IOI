eb07279ca6672bf6bcf028bce25ef5dbffbbb9e6
Parsa Jokar <prsjokar@gmail.com>
2023-02-15 15:47:35 +0330

    CF688B

---
+++ codeforces/688B.cpp
@@ -0,0 +1,17 @@
+// Parsa Jokar 2023 https://github.com/phictus/ioi
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s;
+    for (size_t i = s.size(); i > 0; i--)
+        cout << s[i - 1];
+
+    return (0 ^ 0);
+}
