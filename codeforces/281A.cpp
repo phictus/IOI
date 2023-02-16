e721c8840a01c949d948a8c9de01e0aeb98966f5
Parsa Jokar <prsjokar@gmail.com>
2023-02-16 21:23:18 +0330

    CF281A

---
+++ codeforces/281A.cpp
@@ -0,0 +1,16 @@
+// Parsa Jokar 2023 https://github.com/phictus/ioi
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s << endl;
+
+    return (0 ^ 0);
+}
