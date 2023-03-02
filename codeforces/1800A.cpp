f6cce180a60cb17c255ded1d8d4caa058dc04b64
Parsa Jokar <prsjokar@gmail.com>
2023-03-02 20:32:00 +0330

    CF1800A

---
+++ codeforces/1800A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+string removeDuplicates(const string& str)
+{
+    stringstream stream;
+    char lastC = ' ';
+    for (char c : str)
+    {
+        if (c != lastC)
+        {
+            stream << c;
+            lastC = c;
+        }
+    }
+
+    return stream.str();
+}
+
+int main()
+{
+    uint32_t t;
+    cin >> t;
+    while (t--)
+    {
+        uint32_t n;
+        cin >> n;
+        string str;
+        cin >> str;
+        transform(str.begin(), str.end(), str.begin(), ::tolower);
+        if (removeDuplicates(str) == "meow")
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return (0 ^ 0);
+}
